29c200f3034085acc8d0777edaf910cb91ccbaec
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-18 04:26:05 +0000

    Adding tarfile member sanitization to extractall()

--- majordome/mkpyenv/manager.py
+++ majordome/mkpyenv/manager.py
@@ -330,7 +330,26 @@ class Manager:
                     raise FileNotFoundError(pkg)
 
                 with tarfile.open(pkg, 'r:bz2') as tar_ref:
-                    tar_ref.extractall(self.py_name)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(tar_ref, self.py_name)
             except Exception as err:
                 logger.error(f'While installing {pkg}: {err}')
 
